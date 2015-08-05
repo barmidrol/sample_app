f8bdd45294056747a1f61ac36c751e383b812af6
barmidrol <barmidrol@gmail.com>
2015-08-05 14:37:30 +0300

    Finish sign in

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up testing by decreasing encryption cost
+  ActiveModel::SecurePassword.min_cost = true
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do 
+		name 	"Siarhei Padlozny"
+		email 	"asd@asdasd.ru"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
